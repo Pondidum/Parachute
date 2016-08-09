035fb17915c588089beafc22f0a3b739472b2ecd
andy <andy@andydote.co.uk>
2016-08-09 16:38:28 +0100

    fallback: add basic implementation

---
+++ src/Parachute.Tests/FallbackTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace Parachute.Tests
+{
+	public class FallbackTests
+	{
+		[Fact]
+		public void When_the_first_works()
+		{
+			var first = false;
+			var second = false;
+			var third = false;
+
+			Fallback.Run(
+				() => { first = true; },
+				() => { second = true; },
+				() => { third = true; }
+			);
+
+			first.ShouldBe(true);
+			second.ShouldBe(false);
+			third.ShouldBe(false);
+		}
+
+		[Fact]
+		public void When_the_second_works()
+		{
+			var first = false;
+			var second = false;
+			var third = false;
+
+			Fallback.Run(
+				() => { throw new NotSupportedException(); },
+				() => { second = true; },
+				() => { third = true; }
+			);
+
+			first.ShouldBe(false);
+			second.ShouldBe(true);
+			third.ShouldBe(false);
+		}
+
+		[Fact]
+		public void When_none_work()
+		{
+			var first = false;
+			var second = false;
+			var third = false;
+
+			Should.Throw<NotSupportedException>(() =>
+				Fallback.Run(
+					() => { throw new NotSupportedException(); },
+					() => { throw new NotSupportedException(); },
+					() => { throw new NotSupportedException(); }
+				)
+			);
+
+			first.ShouldBe(false);
+			second.ShouldBe(false);
+			third.ShouldBe(false);
+		}
+	}
+}
---
+++ src/Parachute/Fallback.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Parachute
+{
+	public class Fallback
+	{
+		public static void Run(params Action[] actions)
+		{
+			foreach (var action in actions)
+			{
+				try
+				{
+					action();
+					return;
+				}
+				catch (Exception)
+				{
+					if (action == actions.Last())
+						throw;
+				}
+			}
+		}
+	}
+}
